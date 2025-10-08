6ff3ace453d20062a6681265b49bd2f31c36e535
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-08 12:27:06 -0300

    Definicion de clase y Objeto con sus atibutos y metodos

--- src/poojava/Persona.java
+++ src/poojava/Persona.java
@@ -3,21 +3,31 @@ package poojava;
 
 public class Persona {   // Clase publica persona 
 	// Atributos Privados (estado)
-	private String nombre = "Elias";  
-	private int Edad =  22;
+	private String nombre ;
+	private int edad ;
 	
 	// Sintaxis de declaracin de una  variable en java
 	int dia = 4;
 	// Sintaxis de declaracion  de  una constante publica  en java
 	public static int semana  = 7;
+	
+	// Constructor 
+	public Persona(String nombre , int edad) { 
+		// Se usa al crear objetos 
+		this.nombre = nombre; // this referencia al atributo de la instancia 
+		this.edad = edad;
+	}
 
+	// Metodos (Comportamientos )
+	public void  saludar() {
+		System.out.println("Hola , soy " + nombre  + " y tengo  " + edad +"  "+"esa edad");
+	}
 	public static void main(String[] args) {
-		
 		// Creamos un objeto persona 
-		Persona  p = new Persona();
-		System.out.println( "Hola !! . Tu nombre es : " +p. nombre + " y tu edad es : " +  p.Edad  +  " " +  "años .");
-		System.out.println("SEMANA : " + semana);
-
+		Persona  p = new Persona("Elias ",  22  );
+		p.saludar(); // Llamamos al metodo 
+	
+		
 
 	}
 
