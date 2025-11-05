9aba841e120878169efb8cc1e3f2f641db406201
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-05 19:58:31 -0300

    Creacion de subprocesos

--- src/poojava/CreadorSubproceso.java
+++ src/poojava/CreadorSubproceso.java
@@ -3,9 +3,17 @@ package poojava;
 public class CreadorSubproceso {
 
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Creacion de Subprocesos");
+		Thread subproceso1 = new Thread(new  TareaImprimir("tarea1") );
+		Thread subproceso2 = new Thread(new  TareaImprimir("tarea2") );
+		Thread subproceso3 = new Thread(new  TareaImprimir("tarea 3") );
 		
-		System.out.println();
+		System.out.println("Subprocesos creados , iniciando tareas.");
+		subproceso1.start(); // invoca  al metodo  run de tarea1
+		subproceso2.start(); // invoca al metodo  run de tarea2
+		subproceso3.start(); // invoca al metodo run de  tarea3
+		System.out.println("Tareas iniciadas , main termina .\n");
+		
 	
 	}
 
