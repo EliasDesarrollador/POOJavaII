23f4fadf8df082d61e35849cd6755edc09ddd0d2
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-25 23:17:07 -0300

    Ejercicio de hilos

---
+++ src/hilos/HiloEj.java
@@ -0,0 +1,30 @@
+package hilos;
+
+public class HiloEj {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++)
+			;
+		{
+			System.out.println("Proceso 1 ");
+		}
+		
+		System.out.println(" ");
+
+		for (int i = 0; i <= 5; i++)
+			;
+		{
+			System.out.println("Proceso 2 ");
+		}
+		
+		System.out.println(" ");
+
+		for (int i = 0; i <= 5; i++)
+			;
+		{
+			System.out.println("Proceso 3 ");
+		}
+	}
+
+}
