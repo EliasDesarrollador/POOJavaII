08ca25b8b4beb4b0938cf786c0f1d5ff282cfcee
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-25 23:26:32 -0300

    Primer hilo

--- src/hilos/Porceso1.java
+++ src/hilos/Porceso1.java
@@ -1,12 +1,16 @@
 package hilos;
 
 // Heredamos de la clase pade Thread
-public class Porceso1  extends Thread{
-	
+public class Porceso1 extends Thread {
+
 	@Override
 	public void run() {
-		
-		
+		for (int i = 0; i <= 5; i++)
+			;
+		{
+			System.out.println("Proceso 1 ");
+		}
+
 	}
 
 }
